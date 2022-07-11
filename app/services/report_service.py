47fd7fe701435355d89d05d4781637756b3db0da
SpAnsonLi <ansonli@sharppoint.com.hk>
2022-07-11 09:49:39 +0800

    report update

--- app/services/report_service.py
+++ app/services/report_service.py
@@ -11,30 +11,38 @@ class Report(PnLService):
         self.date = date
         # super class attr: accName, col, tradeNum, totalDoneContract, _pnl
 
+    #get the number of trade
     def __totaltrade(self,list):
         count: int
         for _ in list:
             count = count+1
         return count
+
+    #get the buy cost of pnl to cal the avg. return
     def __get_cost(self):
         data = self.__data_for_pnl
+        pass                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
     
+    #get the total of pnl
     def __get_totalpnl(self, pnlList):
         totalpnl: int
         for i in pnlList:
             totalpnl+=i
         return totalpnl
     
+
+    #use statistics package to cal the sd.
     def __get_sd(self):
         return (statistics.stdev(self))
 
 
 
     @classmethod
-    # def __avg_return(cls, listofpnl):
-    #     avg_return = ((cls.__get_totalpnl(listofpnl) / cls.)*100)
-    #     return avg_return
+    def __avg_return(cls, listofpnl):
+        avg_return = ((cls.__get_totalpnl(listofpnl) / cls.__get_cost)*100)
+        return avg_return
 
+    #store the returns as a list then use statistics to cal the return sd. https://www.geeksforgeeks.org/python-statistics-stdev/ (example)
     def __return_std_dev(self):
         pass
 
@@ -58,10 +66,10 @@ class Report(PnLService):
         "Profits. std. dev.": self.__avg_return_sd (self.__get_totalpnl(positivePnl+negativePnl)),
         "Min. profit": min(pnl),
         "Max. profit": max(pnl),
-        # "Avg. return": self.__avg_return,
-        # "Return std. dev.": self.__return_std_dev,
-        # "Max. Return": self.__max_return,
-        # "Min. Return": self.__min_return
+        "Avg. return": self.__avg_return,
+        "Return std. dev.": self.__return_std_dev,
+        "Max. Return": max(),
+        "Min. Return": min()
         },
         {
         "Profitable trades": self.__get_totalpnl(positivePnl),
@@ -69,10 +77,10 @@ class Report(PnLService):
         "Profits. std. dev.": self.__avg_return_sd (self.__get_totalpnl(positivePnl)),
         "Min. profit": min(positivePnl),
         "Max. profit": max(positivePnl),
-        # "Avg. return": self.__avg_return,
-        # "Return std. dev.": self.__return_std_dev,
-        # "Max. Return": self.__max_return,
-        # "Min. Return": self.__min_return   
+        "Avg. return": self.__avg_return,
+        "Return std. dev.": self.__return_std_dev,
+        "Max. Return": max(),
+        "Min. Return": min()   
         },
         {
         "Unprofitable trades": self.__get_totalpnl(negativePnl),
@@ -80,10 +88,10 @@ class Report(PnLService):
         "Losses. std. dev.": self.__avg_return_sd(self.__get_totalpnl(negativePnl)),
         "Min. Loss": min(negativePnl),
         "Max. Loss": max(negativePnl),
-        # "Avg. return": self.__avg_return,
-        # "Return std. dev.": self.__return_std_dev,
-        # "Max. Return": self.__max_return,
-        # "Min. Return": self.__min_return
+        "Avg. return": self.__avg_return,
+        "Return std. dev.": self.__return_std_dev,
+        "Max. Return": max(),
+        "Min. Return": min()
         }}
         # self.__get_done_trade
         return report
