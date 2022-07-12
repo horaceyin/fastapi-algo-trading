e91d0406e0b3495417bf802587b917bc1f4f7d3d
Horace <horacewong@sharppoin.com.hk>
2022-07-12 15:07:14 +0800

    update backtesting model and add validators

--- app/services/backtesting/backtesting_service.py
+++ app/services/backtesting/backtesting_service.py
@@ -1,4 +1,3 @@
-from os import environ
 import json
 from schemas.backtesting_schemas import BacktestingModel
 from services.backtesting.sma_backtest_service import sma_backtest
@@ -9,11 +8,12 @@ class BacktestingService:
 
     @staticmethod
     def run_backtesting(request: BacktestingModel): # Make request that follows the BacktestingModel format
-        print(request)
-        errMsg = ''
-        # return json.dumps({'msg': 'from backtesting.'}) # Result given
-        # write backtesting code here
-        productCode = request.prodCode
-        portfValue = request.portfolioValue
-        return sma_backtest(productCode, 2, 120, 80, True, portfValue, 1).start_backtesting(request) 
-        # May need to add timeframe and method for data collection
+
+        return request
+
+        # # return json.dumps({'msg': 'from backtesting.'}) # Result given
+        # # write backtesting code here
+        # productCode = request.prodCode
+        # portfValue = request.portfolioValue
+        # return sma_backtest(productCode, 2, 120, 80, True, portfValue, 1).start_backtesting(request) 
+        # # May need to add timeframe and method for data collection
